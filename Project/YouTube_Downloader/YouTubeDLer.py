032c8ddc371ae7726ff7892dc30e8e3ff83eb5b6
Joe <joe.ellis.505@gmail.com>
2013-11-15 11:38:33 -0500

    Added program to download comments, youtube videos, searched videos, and videos from a particular user

--- Project/YouTube_Downloader/YouTubeDLer.py
+++ Project/YouTube_Downloader/YouTubeDLer.py
@@ -11,6 +11,7 @@ import json
 from bs4 import BeautifulSoup
 import urllib, urllib2
 
+
 # Class for interacting with youtube
 class YouTubeUtils():
 	
@@ -25,7 +26,7 @@ class YouTubeUtils():
 
 		return
 
-	def DownloadVideo(self,url):
+	def DownloadVideo(self,vid_id):
 		# This downloads the video
 		execpath = self.path_to_youtubedl
 
@@ -35,8 +36,11 @@ class YouTubeUtils():
 		else:
 			download_dir = self.download_dir + "/"
 
+		# Create the youtube url using the video id
+		url = "http://www.youtube.com/watch?v=" + vid_id
+
 		# This options is what we want to use the 
-		o_option = "'%s%s'" % (download_dir,"%(title)s.%(ext)s")
+		o_option = "'%s%s'" % (download_dir,"%(id)s.%(ext)s")
 
 		output = sub.Popen(execpath + " -o " 
 									+ o_option 
@@ -53,7 +57,9 @@ class YouTubeUtils():
 		# The vid_id is the number pattern that appears in the video webpage
 		comment_feed = self.client.GetYouTubeVideoCommentFeed(video_id=vid_id)
 		video_comments = []
-		while comment_feed is not None:
+		iter_val = 0
+		while comment_feed is not None and (iter_val < 5):
+			iter_val += 1
 			for comment in comment_feed.entry:
 				video_comments.append(comment)
 			next_link = comment_feed.GetNextLink()
@@ -71,6 +77,16 @@ class YouTubeUtils():
 			content = soup.find("ns0:content")
 			if name != None:
 				names_and_comments.append((name.string,content.string))
+
+		# This is the file to output the comments to
+		output_file = os.path.join(self.download_dir,vid_id + ".comments")
+		with open(output_file, "w") as f:
+			for name,comment in names_and_comments:
+				f.write(name.encode("ascii", "ignore"))
+				f.write(" : ")
+				if comment:
+					f.write(comment.encode("ascii", "ignore"))
+				f.write("\n")
 		
 		return names_and_comments
 
@@ -86,22 +102,150 @@ class YouTubeUtils():
 		return
 
 	def GetVidsforUser(self,username):
-		# This function finds the video for a given user
+		# This function finds the video for a given user, adn returns their ids and publish times
+		# TODO: Add 
 		video_user_url = "https://gdata.youtube.com/feeds/api/users/"
 		query_url = video_user_url + username + "/uploads"
-		print query_url
-		print urllib2.urlopen(query_url).read()
-		return
+		returned_videos =  urllib2.urlopen(query_url).read()
 
+		# Now let's parse the returned object with Soup
+		soup = BeautifulSoup(returned_videos)
+		# All we need for each of these is the video id, and published ids
+		ids = soup.find_all("id")
+		publisheds = soup.find_all("published")
+		
+		vid_ids_andtime = []
+		for id_string, published in zip(ids[1:],publisheds):
 
+			id_string = id_string.string
 
+			id_ = id_string[id_string.string.rfind("/")+1:]
+			print id_
+			print published.string
+
+			vid_ids_andtime.append((id_,published.string))
+
+		return vid_ids_andtime
+
+	def SearchforVideos(self, search_terms, order_by_term="viewCount"):
+		# This function will search for videos based on a given search query
+		# Create the query object for the youtube search
+		query = service.YouTubeVideoQuery()
+		query.vq = search_terms
+		query.orderby = order_by_term
+		query.racy = "include"
+		query.max_results = 50
+		returned_results = self.client.YouTubeQuery(query)
+
+		# Now let's parse through the results and get the transcript ids
+		soup = BeautifulSoup(str(returned_results).decode("utf-8"))
+
+		vid_ids = soup.find_all("ns0:id")
+		author_ids = soup.find_all("ns0:uri")
+		published_times = soup.find_all("ns0:published")
+
+		search_vids = []
+		for vid_id,author_id,published_time in zip(vid_ids[1:],author_ids[1:],published_times):
+
+			id_string = vid_id.string
+			author_id_string = author_id.string
+
+			id_ = id_string[id_string.string.rfind("/")+1:]
+			author_id_ = author_id_string[author_id_string.rfind("/")+1:]
+
+			search_vids.append((id_,author_id_,published_time.string))
+
+		return search_vids
+# ENDCLASS YouTubeUtils()
+
+def run(argv):
+	# This is the run function for this bad boy
+	try:
+		opts,args = getopt.getopt(argv,"v:u:s:d:p:hn:")
+	except getopt.GetoptError:
+		print "Usage Error: Please See help"
+		sys.exit(1)
+
+	# Priorities that come first are video_id, user, search
+	path_to_youtubedl = None
+	download_dir = None
+	vid_id = None
+	serach_terms = None
+	user_id = None
+	num_to_download = 10;
+	for opt, arg in opts:
+		if opt in ("-h"):
+			print "YoutTubeDLer.py -- help instructions"
+			print "v: video_id, this is used to download one video"
+			print "u: user_id, this is used to download the videos for a particular user"
+			print "s: search_terms, this queries youtube given the serach terms discussed"
+			print "d: download_dir, the directory to download videos and content too, <MUST BE PROVIDED>"
+			print "p: path_to_youtube-dl_executable: like it sounds, <MUST BE PROVIDED>"
+			sys.exit(1)
+		elif opt in ("-v"):
+			vid_id = arg
+		elif opt in ("-u"):
+			user_id = arg
+		elif opt in ("-s"):
+			search_terms = arg
+		elif opt in ("-d"):
+			download_dir = arg
+		elif opt in ("-p"):
+			path_to_youtubedl = arg
+		elif opt in ("-n"):
+			num_to_download = int(arg)
+
+	# Check to make sure that we have the necessary variables
+	if not (path_to_youtubedl and download_dir):
+		print "You need to supply the path to youtube-dl <p> and download directory <d>"
+		sys.exit(1)
+
+	# Initialize the YouTubeUtils class
+	ydl = YouTubeUtils(path_to_youtubedl, download_dir)
+
+	# This downloads the info, video, and transcript if available
+	if vid_id:
+		print "Downloading Video w/ id: ", vid_id
+		ydl.DownloadVideo(vid_id)
+		ydl.GetCommentsforVideo(vid_id)
+		print "Finished Downloading"
+
+	# This downloads the videos from a user, but for now it stops at 10 for a safeguard
+	elif user_id:
+
+		print "Downloading %d videos from user: %s" % (num_to_download,user_id)
+		# Get the videos for a specified user_id
+		vid_ids_and_times = ydl.GetVidsforUser(user_id)
+
+		for vid_id, pub_time in vid_ids_and_times[0:num_to_download]:
+			ydl.DownloadVideo(vid_id)
+			ydl.GetCommentsforVideo(vid_id)
+
+		print "Finished Downloading"
+
+	elif search_terms:
+
+		print "Downloading videos based on the search term: ", search_terms
+		search_results = ydl.SearchforVideos(search_terms)
+
+		for vid_id, author, pub_time in search_results[0:num_to_download]:
+			ydl.DownloadVideo(vid_id)
+			ydl.GetCommentsforVideo(vid_id)
+
+		print "Finished Downloading"
+	return 
 
 
 ### The Main run portion of the code ###
 if __name__ == "__main__":
-	ydl = YouTubeUtils("./YouTube_Downloader/youtube-dl", "YouTubeVideos")
-	#ydl.DownloadVideo("http://www.youtube.com/watch?v=yiQ4THK5qV8")
+	run(sys.argv[1:])
+
+	# These are for debug testing purposes
+	#ydl = YouTubeUtils("./YouTube_Downloader/youtube-dl", "YouTubeVideos")
+	#ydl.DownloadVideo("lQGDqH6rHII")
 	#ydl.GetCommentsforVideo("lQGDqH6rHII")
 	#ydl.GetTranscriptforVideo("lQGDqH6rHII")
-	ydl.GetVidsforUser("DisneyMovieTrailers")
+	#ydl.GetVidsforUser("ColumbiaDVMM")
+	#ydl.SearchforVideos("Liverpool F.C.")
+
 
